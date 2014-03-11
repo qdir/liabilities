1c897f0552c2bd8da3733455de62a1c611301e49
runix <runix@runix>
2014-03-11 14:35:51 +0100

    added accounts

---
+++ aw_passive_operations/src/es/unileon/aw/account/types/SightAccount.java
@@ -0,0 +1,21 @@
+/* Application developed for AW subject, belonging to passive operations
+ group.*/
+
+package es.unileon.aw.account.types;
+
+import es.unileon.aw.account.Account;
+import es.unileon.aw.bank.Bank;
+import es.unileon.aw.handler.MalformedHandlerException;
+import es.unileon.aw.office.Office;
+
+/**
+ *
+ * @author runix
+ */
+public class SightAccount extends Account{
+
+    public SightAccount(Office office, Bank bank, int accountnumber) throws MalformedHandlerException {
+        super(office, bank, accountnumber);
+    }
+    
+}
