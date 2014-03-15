3339ec6894d69f4a32dbb8f6ae261cd2d5750d2e
erodrg02 <Eva@Eva-PC>
2014-03-15 01:21:59 +0100

    GUI Clients

--- aw_passive_operations/src/es/unileon/aw/GUI/client/ClientsGUI.java
+++ aw_passive_operations/src/es/unileon/aw/GUI/client/ClientsGUI.java
@@ -1,13 +1,10 @@
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
+import java.awt.*;
 import javax.swing.*;
 
 
 public class ClientsGUI extends JFrame {
 
-	 //Declaramos las etiquetas que vamos a utilizar
+    //Declaramos las etiquetas que vamos a utilizar
     private JLabel name;
     private JLabel surname;
     private JLabel address;
@@ -20,7 +17,7 @@ public class ClientsGUI extends JFrame {
     private JLabel phone2;
     private JLabel accountType;
 
-    //Declaramos los campos que vamos a utilizar
+    //Declaramos los campos que vamos a utilizar y seran editables
     private JTextField nameText;
     private JTextField surnameText;
     private JTextField addressText;
@@ -30,11 +27,12 @@ public class ClientsGUI extends JFrame {
     private JTextField birthdayText;
 
     public ClientsGUI(){
+        //Creamos etiqueta y campo
     	name = new JLabel("Nombre: ");
-    	nameText = new JTextField();
+    	nameText = new JTextField(20);
     	
     	surname = new JLabel("Apellidos:");
-    	surnameText = new JTextField(20);
+    	surnameText = new JTextField();
     	
     	DNI= new JLabel("DNI: ");
     	DNIText = new JTextField();
@@ -42,22 +40,31 @@ public class ClientsGUI extends JFrame {
     	birthday = new JLabel("Fecha de nacimiento (dd/mm/aa): ");
     	birthdayText = new JTextField();
     	
+        //Asignamos a la ventana principal el layout GridBagLayout
     	this.setLayout(new GridBagLayout());
+        //tamaño de la ventana
     	this.setSize(600, 200);
+        //No se podra modificar el tamaño
     	this.setResizable(false);
+        
+        //Creamos los ajustes para el layout. Determinan la posicion de los elementos
     	GridBagConstraints constr=new GridBagConstraints();
-    	
+    	/*Fill: indica cuanto se estira un componente.
+        anchor: Indica la posicion
+        insets: margenes respecto a los otros componentes
+        gridwidth: ancho del componente (numero de filas que ocupa)
+        gridheight: alto del componente
+        http://www.chuidiang.com/java/layout/GridBagLayout/GridBagLayout.php
+        */
+        
     	//Nombre
     	constr.fill = GridBagConstraints.HORIZONTAL;
     	constr.anchor=GridBagConstraints.WEST;
     	constr.insets=new Insets(0,0,0,0);
     	constr.gridwidth=1;
-        this.add(name, constr);
+        this.add(name, constr); //Añadimos el elemento y sus propiedades para que se situe donde queremos
         constr.gridwidth = GridBagConstraints.REMAINDER;
         add(nameText, constr);
-        
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setVisible(true);
         
         //Apellidos
         constr.fill=GridBagConstraints.HORIZONTAL;
@@ -86,11 +93,16 @@ public class ClientsGUI extends JFrame {
         constr.gridwidth = GridBagConstraints.REMAINDER;
         add(birthdayText, constr);
         
+        //Faltan campos
+        //Falta opcion elegir tipo de cuenta (desplegable)
         
+        //Accion al cerrar la ventana
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
     
+    
     public static void main(String[] args) {
-        //TODO
         ClientsGUI gui = new ClientsGUI();
     }
 }
