46e4d22a7a101f464179c8d9ea47f5da2b71eadc
runix <runix@runix>
2014-03-20 17:45:32 +0100

    Added titulars to account

--- aw_passive_operations/src/es/unileon/ulebank/account/Account.java
+++ aw_passive_operations/src/es/unileon/ulebank/account/Account.java
@@ -4,12 +4,15 @@ import es.unileon.ulebank.account.exception.BalanceException;
 import es.unileon.ulebank.account.handler.AccountHandler;
 import es.unileon.ulebank.account.history.AccountHistory;
 import es.unileon.ulebank.bank.Bank;
+import es.unileon.ulebank.client.Client;
 import es.unileon.ulebank.office.Office;
 import es.unileon.ulebank.handler.Handler;
 import es.unileon.ulebank.handler.MalformedHandlerException;
 import es.unileon.ulebank.history.History;
 import es.unileon.ulebank.history.Transaction;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  *
@@ -25,7 +28,10 @@ public abstract class Account {
      * The amount of money of the account
      */
     private float balance;
-
+    /**
+     * The account titulars
+     */
+    private List<Client> titulars;
     /**
      * The history of the account
      */
@@ -47,6 +53,52 @@ public abstract class Account {
         this.id = new AccountHandler(office.getID(), bank.getID(), accountnumber);
         this.history = new AccountHistory();
         this.balance = 0.0f;
+        this.titulars = new ArrayList<>();
+    }
+
+    /**
+     *
+     * Add a new titular
+     *
+     * @param client ( client to add)
+     *
+     * @return ( true if success, else false )
+     */
+    public boolean addTitular(Client client) {
+        for (int i = 0; i < this.titulars.size(); i++) {
+            if (this.titulars.get(i).getId().compareTo(client.getId()) == 0) {
+                return false;
+            }
+        }
+        this.titulars.add(client);
+        return true;
+    }
+
+    /**
+     *
+     * Delete a titular
+     *
+     * @param id ( The client id )
+     *
+     * @return ( true if success, else false )
+     */
+    public boolean deleteTitular(Handler id) {
+        for (int i = 0; i < this.titulars.size(); i++) {
+            if (this.titulars.get(i).getId().compareTo(id) == 0) {
+                this.titulars.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the account titulars
+     *
+     * @return ( The titulars )
+     */
+    public List<Client> getTitulars() {
+        return new ArrayList<>(this.titulars);
     }
 
     /**
