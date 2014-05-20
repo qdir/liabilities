20b786d144b583a5c261c01d7b29e0d94cccb6bc
runix <rrodrf04@estudiantes.unileon.es>
2014-05-20 12:12:29 +0200

    Updated Address tests

--- src/test/java/es/unileon/ulebank/client/AddressTest.java
+++ src/test/java/es/unileon/ulebank/client/AddressTest.java
@@ -55,63 +55,114 @@ public class AddressTest {
 		this.door = 'A';
 		this.floor = 3;
 		this.street = "Avn Universidad";
-		this.address = new Address(street, blockNumber, floor, door, locality, province, zipCode);
+		this.address = new Address(street, blockNumber, floor, door, locality,
+				province, zipCode);
 	}
-	
+
 	@Test
 	public void testGetAndSetProvince() {
-		assertEquals(this.province.equals(this.address.getProvince()),0);
+		assertEquals(this.province.equals(this.address.getProvince()), true);
 		String other = "other";
 		this.address.setProvince(other);
-		assertEquals(other.equals(this.address.getProvince()),0);
+		assertEquals(other.equals(this.address.getProvince()), true);
+		other = "ElBierzo";
+		this.address.setProvince(other);
+		assertEquals(other.equals(this.address.getProvince()), true);
+		other = "Zamara";
+		this.address.setProvince(other);
+		assertEquals(other.equals(this.address.getProvince()), true);
 	}
-	
+
 	@Test
 	public void testGetAndSetLocality() {
-		assertEquals(this.locality.equals(this.address.getLocality()),0);
+		assertEquals(this.locality.equals(this.address.getLocality()), true);
 		String other = "other";
 		this.address.setLocality(other);
-		assertEquals(other.equals(this.address.getLocality()),0);
-		
+		assertEquals(other.equals(this.address.getLocality()), true);
+		other = "Bembibre";
+		this.address.setLocality(other);
+		assertEquals(other.equals(this.address.getLocality()), true);
+		other = "Ponferrada";
+		this.address.setLocality(other);
+		assertEquals(other.equals(this.address.getLocality()), true);
+
 	}
-	
+
 	@Test
 	public void testGetAndSetStreet() {
-		assertEquals(this.street.equals(this.address.getStreet()),0);
+		assertEquals(this.street.equals(this.address.getStreet()), true);
 		String other = "other";
 		this.address.setStreet(other);
-		assertEquals(other.equals(this.address.getStreet()),0);
+		assertEquals(other.equals(this.address.getStreet()), true);
+		other = "Avn Universidad";
+		this.address.setStreet(other);
+		assertEquals(other.equals(this.address.getStreet()), true);
+		other = "La condesa";
+		this.address.setStreet(other);
+		assertEquals(other.equals(this.address.getStreet()), true);
 	}
-	
+
 	@Test
 	public void testGetAndSeZipCode() {
 		assertEquals(this.zipCode, this.address.getZipCode());
 		int newZip = 10;
 		this.address.setZipCode(newZip);
 		assertEquals(newZip, this.address.getZipCode());
+		newZip = 12;
+		this.address.setZipCode(newZip);
+		assertEquals(newZip, this.address.getZipCode());
+		newZip = 110;
+		this.address.setZipCode(newZip);
+		assertEquals(newZip, this.address.getZipCode());
+		newZip = 1201300;
+		this.address.setZipCode(newZip);
+		assertEquals(newZip, this.address.getZipCode());
 	}
-	
+
 	@Test
 	public void testGetAndSetFloor() {
 		assertEquals(this.floor, this.address.getFloor());
 		int newFloor = 3;
 		this.address.setFloor(newFloor);
 		assertEquals(newFloor, this.address.getFloor());
+		newFloor = 103;
+		this.address.setFloor(newFloor);
+		assertEquals(newFloor, this.address.getFloor());
+		newFloor = 3222;
+		this.address.setFloor(newFloor);
+		assertEquals(newFloor, this.address.getFloor());
+		newFloor = 3101;
+		this.address.setFloor(newFloor);
+		assertEquals(newFloor, this.address.getFloor());
 	}
-	
-	
+
 	@Test
 	public void testGetAndSetDoor() {
 		assertEquals(this.door, this.address.getDoor());
 		char newDoor = 'Z';
 		this.address.setDoor(newDoor);
 		assertEquals(newDoor, this.address.getDoor());
+		newDoor = 'A';
+		this.address.setDoor(newDoor);
+		assertEquals(newDoor, this.address.getDoor());
+		newDoor = 'U';
+		this.address.setDoor(newDoor);
+		assertEquals(newDoor, this.address.getDoor());
 	}
-	
+
 	@Test
 	public void testGetAndSetBlockNumber() {
 		assertEquals(this.blockNumber, this.address.getBlockNumber());
 		int newBlockNumber = 3;
+		this.address.setBlockNumber(newBlockNumber);
+		assertEquals(newBlockNumber, this.address.getBlockNumber());
+		newBlockNumber = 30;
+		this.address.setBlockNumber(newBlockNumber);
+		assertEquals(newBlockNumber, this.address.getBlockNumber());
+		newBlockNumber = 130;
+		this.address.setBlockNumber(newBlockNumber);
+		assertEquals(newBlockNumber, this.address.getBlockNumber());
+		newBlockNumber = 232;
 		this.address.setBlockNumber(newBlockNumber);
 		assertEquals(newBlockNumber, this.address.getBlockNumber());
 	}
