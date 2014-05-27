a95bff8c6ea8aa7f7632bd57f64188fd029f7c51
runix <rrodrf04@estudiantes.unileon.es>
2014-05-27 10:12:36 +0200

    Refactor test name

--- src/test/java/es/unileon/ulebank/account/liquidation/doubleFeatureExtractors/DoubleFeatureExtractorPayrrolsNumberTest.java
+++ src/test/java/es/unileon/ulebank/account/liquidation/doubleFeatureExtractors/DoubleFeatureExtractorPayrrolsNumberTest.java
@@ -21,7 +21,7 @@ import es.unileon.ulebank.history.TransactionHandlerProvider;
 import es.unileon.ulebank.history.conditions.WrongArgsException;
 import es.unileon.ulebank.office.Office;
 
-public class DoubleFeatureExtractorPayrrolSNumbertest {
+public class DoubleFeatureExtractorPayrrolsNumberTest {
 
 	private String subject;
 
